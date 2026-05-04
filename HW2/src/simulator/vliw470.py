e944b509e015634dcccd6525cd59c2beb636f4fb
k4lizen <124312252+k4lizen@users.noreply.github.com>
2026-05-04 15:56:41 +0200

    ruff format python files, delete and gitignore pycache

--- HW2/src/simulator/vliw470.py
+++ HW2/src/simulator/vliw470.py
@@ -5,21 +5,23 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "instructions", type=argparse.FileType("r"), 
-    help="The JSON file defining the instruction to be executed"
+    "instructions",
+    type=argparse.FileType("r"),
+    help="The JSON file defining the instruction to be executed",
 )
 parser.add_argument(
-    "result", type=argparse.FileType("w"),
-    help="The cycle-accurate simulation result."
+    "result", type=argparse.FileType("w"), help="The cycle-accurate simulation result."
 )
 parser.add_argument(
-    "--memory", type=argparse.FileType("r"),
-    help="Optional data memory JSON initialization file."
+    "--memory",
+    type=argparse.FileType("r"),
+    help="Optional data memory JSON initialization file.",
 )
 
 arg = parser.parse_args()
 
 instructionMemory: list[list[str]] = json.load(arg.instructions)
+
 
 class DataMemory:
     data = {}
@@ -30,7 +32,7 @@ class DataMemory:
                 self.data[int(addr, 16)] = data
             else:
                 self.data[int(addr)] = data
-    
+
     def read(self, addr: int) -> int:
         if self.data.get(addr) is None:
             return 0
@@ -39,6 +41,7 @@ class DataMemory:
 
     def write(self, addr: int, data: int) -> int:
         self.data[addr] = data
+
 
 if arg.memory:
     dataMemory = DataMemory(json.load(arg.memory))
@@ -66,27 +69,22 @@ class VLIW470:
     # Functional pipelines
     ALU0Pipe = {
         "predicate": False,
-        "opcode": "alu", # alu, updateLC, updateEC, updateRBB, updatePredicate
+        "opcode": "alu",  # alu, updateLC, updateEC, updateRBB, updatePredicate
         "targetReg": 0,
-        "value": 0
+        "value": 0,
     }
 
-    ALU1Pipe = {
-        "predicate": False,
-        "opcode": "",
-        "targetReg": 0,
-        "value": 0
-    }
+    ALU1Pipe = {"predicate": False, "opcode": "", "targetReg": 0, "value": 0}
 
     BranchPipe = {
         "predicate": False,
-        "opcode": "hw", # lw or loop
+        "opcode": "hw",  # lw or loop
         "targetPC": 0,
     }
 
     MemoryPipe = {
         "predicate": False,
-        "opcode": "load", # load or store
+        "opcode": "load",  # load or store
         "address": 0,
         "data": 0,
         "loadDestReg": 0,
@@ -102,7 +100,7 @@ class VLIW470:
             "predicate": False,
             "targetReg": 0,
             "result": 0,
-        }
+        },
     ]
 
     def serialize(self) -> dict:
@@ -118,17 +116,21 @@ class VLIW470:
             "Branch": self.BranchPipe.copy(),
             "Memory": self.MemoryPipe.copy(),
             "Multiply": self.MultiplierPipe.copy(),
-            "MemoryData": dataMemory.data.copy()
+            "MemoryData": dataMemory.data.copy(),
         }
 
     _debug_currentCycleUpdate = []
 
     def updateRegister(self, name: str, value: int):
         if name in self._debug_currentCycleUpdate:
-            print("Warning: Multiple instructions are updating the register {}.".format(name))
+            print(
+                "Warning: Multiple instructions are updating the register {}.".format(
+                    name
+                )
+            )
         else:
             self._debug_currentCycleUpdate.append(name)
-        
+
         if name.startswith("x"):
             self.PhysicalRegisterFile[int(name[1:])] = value
         elif name.startswith("p"):
@@ -144,19 +146,21 @@ class VLIW470:
 
     def renameRegister(self, idx: int) -> int:
 
-        assert idx >= 0 and idx < 96, "Trying to rename a register out of the specific range."
+        assert idx >= 0 and idx < 96, (
+            "Trying to rename a register out of the specific range."
+        )
 
         if idx >= 32:
             potential = idx - self.RBB
             if potential < 32:
                 return potential + 64
             return potential
-        
+
         return idx
 
     def parseImmediate(self, i: str) -> int:
         if i.startswith("0x"):
-            return int(i, 16) # at present we only support hex and decimal.
+            return int(i, 16)  # at present we only support hex and decimal.
         return int(i)
 
     def parse(self, i: str) -> dict:
@@ -171,7 +175,7 @@ class VLIW470:
             assert idx <= 95, "Undefined predicate register: p{}".format(idx)
             predication = self.PredicateRegisters[self.renameRegister(idx)]
             info.remove(info[0])
-        
+
         operands: list[str] = []
 
         for item in info[1:]:
@@ -184,7 +188,7 @@ class VLIW470:
         return {
             "predicate": predication,
             "opcode": info[0].strip(),
-            "operands": operands
+            "operands": operands,
         }
 
     def decodeALUInstruction(self, i: str) -> dict:
@@ -192,41 +196,53 @@ class VLIW470:
         predication: bool = decoded["predicate"]
         opcode: str = decoded["opcode"]
         ops: list[str] = decoded["operands"]
-        
+
         # Now start the normal instruction decoding.
-        assert opcode in ["add", "addi", "sub", "mov", "nop"], "Undefined instruction: {}".format(i)
-        
+        assert opcode in ["add", "addi", "sub", "mov", "nop"], (
+            "Undefined instruction: {}".format(i)
+        )
+
         # classify instruction by its type
         if opcode in ["add", "addi", "sub"]:
             # define the source
-            assert ops[0].startswith('x'), "Cannot determine the destination: {}".format(i)
-            dest =  self.renameRegister(int(ops[0][1:]))
+            assert ops[0].startswith("x"), (
+                "Cannot determine the destination: {}".format(i)
+            )
+            dest = self.renameRegister(int(ops[0][1:]))
 
-            assert ops[1].startswith('x'), "Cannot determine the source reg: {}".format(i)
+            assert ops[1].startswith("x"), "Cannot determine the source reg: {}".format(
+                i
+            )
             src1 = self.renameRegister(int(ops[1][1:]))
 
             if opcode == "addi":
                 src2 = int(ops[2])
             else:
-                assert ops[2].startswith('x'), "Cannot determine the 2nd source reg: {}".format(i)
+                assert ops[2].startswith("x"), (
+                    "Cannot determine the 2nd source reg: {}".format(i)
+                )
                 src2 = self.renameRegister(int(ops[2][1:]))
 
             result = 0
             if opcode == "add":
-                result = self.PhysicalRegisterFile[src1] + self.PhysicalRegisterFile[src2]
+                result = (
+                    self.PhysicalRegisterFile[src1] + self.PhysicalRegisterFile[src2]
+                )
             elif opcode == "addi":
                 result = self.PhysicalRegisterFile[src1] + src2
             elif opcode == "sub":
-                result = self.PhysicalRegisterFile[src1] - self.PhysicalRegisterFile[src2]
+                result = (
+                    self.PhysicalRegisterFile[src1] - self.PhysicalRegisterFile[src2]
+                )
                 if result < 0:
-                    result = result + 0x10000000000000000 # 2-complementary
+                    result = result + 0x10000000000000000  # 2-complementary
             result = result & 0xFFFFFFFFFFFFFFFF
 
             return {
                 "predicate": predication,
-                "opcode": "alu", # alu, updateLC, updateEC, updateRBB, updatePredicate
+                "opcode": "alu",  # alu, updateLC, updateEC, updateRBB, updatePredicate
                 "targetReg": dest,
-                "value": result
+                "value": result,
             }
 
         elif opcode == "mov":
@@ -237,16 +253,16 @@ class VLIW470:
                 if ops[1] == "true":
                     return {
                         "predicate": predication,
-                        "opcode": "updatePredicate", # alu, updateLC, updateEC, updateRBB, updatePredicate
+                        "opcode": "updatePredicate",  # alu, updateLC, updateEC, updateRBB, updatePredicate
                         "targetReg": idx,
-                        "value": 1
+                        "value": 1,
                     }
                 elif ops[1] == "false":
                     return {
                         "predicate": predication,
-                        "opcode": "updatePredicate", # alu, updateLC, updateEC, updateRBB, updatePredicate
+                        "opcode": "updatePredicate",  # alu, updateLC, updateEC, updateRBB, updatePredicate
                         "targetReg": idx,
-                        "value": 0
+                        "value": 0,
                     }
                 else:
                     assert False, "Cannot determine the source operand: {}".format(i)
@@ -254,38 +270,42 @@ class VLIW470:
                 dest = ops[0].upper()
                 value = int(ops[1])
                 if ops[0].upper() == "RBB":
-                    assert value < 64, "The maximum value of RBB is 63. The value you provide causes overflow."
+                    assert value < 64, (
+                        "The maximum value of RBB is 63. The value you provide causes overflow."
+                    )
                 return {
                     "predicate": predication,
-                    "opcode": "update{}".format(dest), # alu, updateLC, updateEC, updateRBB, updatePredicate
+                    "opcode": "update{}".format(
+                        dest
+                    ),  # alu, updateLC, updateEC, updateRBB, updatePredicate
                     "targetReg": 0,
-                    "value": value
+                    "value": value,
                 }
-            elif ops[0].startswith('x'):
+            elif ops[0].startswith("x"):
                 dst = self.renameRegister(int(ops[0][1:]))
-                if ops[1].startswith('x'):
+                if ops[1].startswith("x"):
                     src = self.renameRegister(int(ops[1][1:]))
                     return {
                         "predicate": predication,
                         "opcode": "alu",
                         "targetReg": dst,
-                        "value": self.PhysicalRegisterFile[src]
+                        "value": self.PhysicalRegisterFile[src],
                     }
-                else: # It should be an integer
+                else:  # It should be an integer
                     return {
                         "predicate": predication,
                         "opcode": "alu",
                         "targetReg": dst,
-                        "value": self.parseImmediate(ops[1])
+                        "value": self.parseImmediate(ops[1]),
                     }
             else:
                 assert False, "Unknown instruction: {}".format(i)
         elif opcode == "nop":
             return {
                 "predicate": False,
-                "opcode": "alu", # alu, updateLC, updateEC, updateRBB, updatePredicate
+                "opcode": "alu",  # alu, updateLC, updateEC, updateRBB, updatePredicate
                 "targetReg": 0,
-                "value": 0
+                "value": 0,
             }
 
     def decodeMultiplierInstruction(self, i: str) -> dict:
@@ -297,14 +317,21 @@ class VLIW470:
         assert opcode in ["mulu", "nop"]
 
         if opcode == "mulu":
-            assert ops[0].startswith('x') and ops[1].startswith('x') and ops[2].startswith('x'), "Undefined instruction: {}".format(i)
+            assert (
+                ops[0].startswith("x")
+                and ops[1].startswith("x")
+                and ops[2].startswith("x")
+            ), "Undefined instruction: {}".format(i)
             dest = self.renameRegister(int(ops[0][1:]))
             src1 = self.renameRegister(int(ops[1][1:]))
             src2 = self.renameRegister(int(ops[2][1:]))
             return {
                 "predicate": predication,
                 "targetReg": dest,
-                "result": (self.PhysicalRegisterFile[src1] * self.PhysicalRegisterFile[src2]) & 0xFFFFFFFFFFFFFFFF,
+                "result": (
+                    self.PhysicalRegisterFile[src1] * self.PhysicalRegisterFile[src2]
+                )
+                & 0xFFFFFFFFFFFFFFFF,
             }
         else:
             return {
@@ -312,7 +339,6 @@ class VLIW470:
                 "targetReg": 0,
                 "result": 0,
             }
-
 
     def decodeLoadStoreInstruction(self, i: str) -> dict:
         decoded = self.parse(i)
@@ -325,7 +351,7 @@ class VLIW470:
         if opcode == "nop":
             return {
                 "predicate": False,
-                "opcode": "load", # load or store
+                "opcode": "load",  # load or store
                 "address": 0,
                 "data": 0,
                 "loadDestReg": 0,
@@ -333,7 +359,9 @@ class VLIW470:
 
         # ops[0]: xNN
         # ops[1]: imm(xMM)
-        assert ops[0].startswith('x') and "(" in ops[1] and ")" in ops[1], "Undefined instruction: {}".format(i)
+        assert ops[0].startswith("x") and "(" in ops[1] and ")" in ops[1], (
+            "Undefined instruction: {}".format(i)
+        )
 
         dest = self.renameRegister(int(ops[0][1:]))
         imm = ops[1].split("(")[0].strip()
@@ -350,7 +378,7 @@ class VLIW470:
         if opcode == "ld":
             return {
                 "predicate": predication,
-                "opcode": "load", # load or store
+                "opcode": "load",  # load or store
                 "address": addr,
                 "data": 0,
                 "loadDestReg": dest,
@@ -358,13 +386,11 @@ class VLIW470:
         elif opcode == "st":
             return {
                 "predicate": predication,
-                "opcode": "store", # load or store
+                "opcode": "store",  # load or store
                 "address": addr,
                 "data": self.PhysicalRegisterFile[dest],
                 "loadDestReg": 0,
             }
-            
-
 
     def decodeBrancInstruction(self, i: str) -> dict:
         decoded = self.parse(i)
@@ -372,24 +398,26 @@ class VLIW470:
         opcode: str = decoded["opcode"]
         ops: list[str] = decoded["operands"]
 
-        assert opcode in ["loop", "loop.pip", "nop"], "Undefined instruction: {}".format(i)
+        assert opcode in ["loop", "loop.pip", "nop"], (
+            "Undefined instruction: {}".format(i)
+        )
 
         if opcode == "loop":
             return {
                 "predicate": predication,
-                "opcode": "loop", # lw or loop
+                "opcode": "loop",  # lw or loop
                 "targetPC": int(ops[0]),
             }
         elif opcode == "loop.pip":
             return {
                 "predicate": predication,
-                "opcode": "hw", # lw or loop
+                "opcode": "hw",  # lw or loop
                 "targetPC": int(ops[0]),
             }
 
         return {
             "predicate": False,
-            "opcode": "hw", # lw or loop
+            "opcode": "hw",  # lw or loop
             "targetPC": 0,
         }
 
@@ -399,9 +427,9 @@ class VLIW470:
             inst = ["nop", "nop", "nop", "nop", "nop"]
         else:
             inst = instructionMemory[self.PC]
-        
+
         assert len(inst) == 5, "Each bundle should always have 5 instructions"
-    
+
         # Branch Unit will be immediately updated, because its' combinational logic.
         #### inst[0] -> ALU0
         self.ALU0Pipe = self.decodeALUInstruction(inst[0])
@@ -437,20 +465,19 @@ class VLIW470:
                 elif aluPipe["opcode"] == "updatePredicate":
                     self.updateRegister("p{}".format(idx), value)
                 else:
-                    assert False, "Wrong opcode is provided: {}".format(aluPipe["opcode"])
-        
+                    assert False, "Wrong opcode is provided: {}".format(
+                        aluPipe["opcode"]
+                    )
+
         #### Memory
         if self.MemoryPipe["predicate"]:
             if self.MemoryPipe["opcode"] == "load":
                 self.updateRegister(
                     "x{}".format(self.MemoryPipe["loadDestReg"]),
-                    dataMemory.read(self.MemoryPipe["address"])
+                    dataMemory.read(self.MemoryPipe["address"]),
                 )
             elif self.MemoryPipe["opcode"] == "store":
-                dataMemory.write(
-                    self.MemoryPipe["address"],
-                    self.MemoryPipe["data"]
-                )
+                dataMemory.write(self.MemoryPipe["address"], self.MemoryPipe["data"])
             else:
                 assert False, "Wrong opcode"
 
@@ -459,7 +486,7 @@ class VLIW470:
         if self.MultiplierPipe[2]["predicate"]:
             self.updateRegister(
                 "x{}".format(self.MultiplierPipe[2]["targetReg"]),
-                self.MultiplierPipe[2]["result"]
+                self.MultiplierPipe[2]["result"],
             )
         self.MultiplierPipe.pop()
 
@@ -468,12 +495,12 @@ class VLIW470:
             self.PC = self.PC
         else:
             self.PC = self.PC + 1
-        
+
         if self.BranchPipe["predicate"]:
             if self.BranchPipe["opcode"] == "loop":
                 if self.LC > 0:
                     self.updateRegister("LC", self.LC - 1)
-                    self.PC  = self.BranchPipe["targetPC"]
+                    self.PC = self.BranchPipe["targetPC"]
             elif self.BranchPipe["opcode"] == "hw":
                 if self.LC > 0:
                     self.updateRegister("LC", self.LC - 1)
@@ -488,8 +515,6 @@ class VLIW470:
                 else:
                     self.updateRegister("p{}".format(self.renameRegister(32)), 0)
 
-        
-
 
 def main():
     processor = VLIW470()
@@ -503,12 +528,9 @@ def main():
             processor.tick()
             processor.tick()
             break
-        
+
     # Finally, dump the state to the file
     json.dump(state, arg.result, indent=4)
-
-
-
 
 
 if __name__ == "__main__":
